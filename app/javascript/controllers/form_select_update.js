a2862a47af79f499c7145cf63178b55fd49a5207
joigs <jose.saez2001@alumnos.ubiobio.cl>
2024-01-27 22:09:04 -0300

    ocurrio un error sin razon y se arreglo sin razon, esto es un llamado de auxilio estoy cayendo en la locura solo dios sabe que paso y dependo de el para que no vuelva a pasar

--- app/javascript/controllers/form_select_update.js
+++ app/javascript/controllers/form_select_update.js
@@ -1,19 +1,22 @@
-//al seleccionar una empresa mandante se actualiza el select de empresas menores, en form de items
 document.addEventListener('DOMContentLoaded', function() {
     const principalSelect = document.querySelector('select[name="item[principal_id]"]');
     const minorSelect = document.querySelector('select[name="item[minor_id]"]');
 
-    principalSelect.addEventListener('change', function() {
-        fetch(`/principals/${this.value}/minors.json`)
-            .then(response => response.json())
-            .then(data => {
-                minorSelect.innerHTML = '';
-                data.forEach(minor => {
-                    const option = document.createElement('option');
-                    option.value = minor.id;
-                    option.text = minor.name;
-                    minorSelect.appendChild(option);
+    if (principalSelect) {
+        principalSelect.addEventListener('change', function() {
+            fetch(`/principals/${this.value}/minors.json`)
+                .then(response => response.json())
+                .then(data => {
+                    minorSelect.innerHTML = '';
+                    data.forEach(minor => {
+                        const option = document.createElement('option');
+                        option.value = minor.id;
+                        option.text = minor.name;
+                        minorSelect.appendChild(option);
+                    });
                 });
-            });
-    });
+        });
+    } else {
+        console.log('principalSelect is null');
+    }
 });
