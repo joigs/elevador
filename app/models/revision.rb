33c29640f68637426d0033e2a6dd2992e0a0b3cd
joigs <jose.saez2001@alumnos.ubiobio.cl>
2024-02-05 12:36:57 -0300

    cambio

--- app/models/revision.rb
+++ app/models/revision.rb
@@ -9,8 +9,10 @@ class Revision < ApplicationRecord
   belongs_to :inspection
 
   def only_owner?
-    self.inspection.user_id == Current.user&.id or Current.user&.admin?
+    inspection = Inspection.find_by(id: self.inspection_id)
+    inspection.user_id == Current.user&.id or Current.user&.admin?
   end
+
 
 
 
