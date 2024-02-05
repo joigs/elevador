154340b2a12909c0ae402f6d85455172685c6a45
joigs <jose.saez2001@alumnos.ubiobio.cl>
2024-02-05 09:56:35 -0300

    cambiar

--- app/javascript/controllers/form_select_update.js
+++ app/javascript/controllers/form_select_update.js
@@ -1,26 +1,24 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-    let principalSelect = document.getElementById('principalSelect');
-    let minorSelect = document.getElementById('minorSelect'); // Assuming the id of the minor select element is 'minorSelect'
-
-    if (principalSelect) {
-        fetch(`/principals/${principalSelect.value}/minors.json`)
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error(`HTTP error! status: ${response.status}`);
-                }
-                return response.json();
-            })
-            .then(data => {
-                minorSelect.innerHTML = '';
-                data.forEach(minor => {
-                    const option = document.createElement('option');
-                    option.value = minor.id;
-                    option.text = minor.name;
-                    minorSelect.appendChild(option);
-                });
-            })
-            .catch(error => {
-                console.log('Fetch Error: ', error);
-            });
+document.addEventListener('turbo:load', function() {
+    const form = document.querySelector('form');
+    if (form) {
+        form.addEventListener('change', function(event) {
+            const principalSelect = event.target.closest('select[name="inspection[item_attributes][principal_id]"]');
+            const minorSelect = document.querySelector('select[name="inspection[item_attributes][minor_id]"]');
+            if (principalSelect) {
+                fetch(`/principals/${principalSelect.value}/minors.json`)
+                    .then(response => response.json())
+                    .then(data => {
+                        minorSelect.innerHTML = '';
+                        data.forEach(minor => {
+                            const option = document.createElement('option');
+                            option.value = minor.id;
+                            option.text = minor.name;
+                            minorSelect.appendChild(option);
+                        });
+                    });
+            }
+        });
+    } else {
+        console.log('form is null');
     }
 });
