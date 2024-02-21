3445c29d1885980db01b1d366eee2b13e6cebebe
joigs <jose.saez2001@alumnos.ubiobio.cl>
2024-02-21 01:48:02 -0300

    se añade las inspecciones en un perfil

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,14 @@
 class UsersController < ApplicationController
   def show
     @user = User.find_by!(username: params[:username])
-    @pagy, @inspections = pagy_countless(FindInspections.new.call({ user_id: @user.id}), items: 10)
+    if @user.admin
+      @inspections = Inspection.where(state: "Cerrado")
+      @pagy, @inspections = pagy_countless(@inspections, items: 10  )
+    else
+      @inspections = Inspection.where(user_id: @user.id)
+      @pagy, @inspections = pagy_countless(@inspections, items: 10  )
+    end
+
+
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
     resources :sessions, only: [:new, :create, :destroy], path: '/login', path_names: { new: '/' }
   end
 
-  resources :users, only: :show, path: '/user', param: :username
+  resources :users, only: :show, path: '/user', param: :username, as: 'perfil'
   resources :inspections, path: '/inspections' do
     member do
       get :generate_document
