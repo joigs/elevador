08730dd794806c3833aa6fbb0cee041f86eb625a
joigs <jose.saez2001@alumnos.ubiobio.cl>
2024-05-07 17:23:40 -0400

    cambio de imagen en informe

--- app/controllers/revisions_controller.rb
+++ app/controllers/revisions_controller.rb
@@ -324,7 +324,7 @@ class RevisionsController < ApplicationController
   def process_image(upload)
     ImageProcessing::MiniMagick
       .source(upload)
-      .resize_to_fit(400, 250)
+      .resize_to_fit(350, 200)
       .call
   end
 
