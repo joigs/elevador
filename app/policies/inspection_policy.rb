b2a52d8c91f067fb7cf69f769fb8652bdc3baa21
joigs <jose.saez2001@alumnos.ubiobio.cl>
2024-08-05 13:02:55 -0400

    ahora el inspector puede modificar algunas cosas de la inspeccion

--- app/policies/inspection_policy.rb
+++ app/policies/inspection_policy.rb
@@ -1,23 +1,18 @@
 class InspectionPolicy < BasePolicy
   def edit
-    Current.user.admin
+    Current.user.admin || record.users.exists?(id: Current.user&.id)
   end
 
   def update
-    Current.user.admin
+    Current.user.admin || record.users.exists?(id: Current.user&.id)
   end
 
   def destroy
     Current.user.admin
   end
 
-  def edit_identificador
-    Current.user.admin
-  end
 
-  def update_identificador
-    Current.user.admin
-  end
+
 
   def update_ending
     Current.user.admin || record.users.exists?(id: Current.user&.id)
