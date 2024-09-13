5fbb1f72c1d091149dd271388fb0af76504d309e
joigs <jose.saez2001@alumnos.ubiobio.cl>
2024-09-13 14:42:22 -0300

    al parecer añadi flowbite

--- config/importmap.rb
+++ config/importmap.rb
@@ -9,3 +9,6 @@ pin "slim-select" # @2.8.1
 pin "tom-select" # @2.3.1
 pin "stimulus-rails-nested-form" # @4.1.0
 pin_all_from "app/javascript/custom", under: "custom"
+pin "flowbite" # @2.5.1
+pin "@popperjs/core", to: "@popperjs--core.js" # @2.11.8
+pin "flowbite-datepicker" # @1.3.0
