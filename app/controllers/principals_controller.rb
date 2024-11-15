f6d43374ad92185e6cc772a1d0feacbcbd286138
joigs <jose.saez2001@alumnos.ubiobio.cl>
2024-11-15 12:53:54 -0300

    debug de los graficos

--- app/controllers/principals_controller.rb
+++ app/controllers/principals_controller.rb
@@ -43,16 +43,16 @@ class PrincipalsController < ApplicationController
     }
 
     # Agrupar inspecciones por mes del año seleccionado
-    @inspections_by_month = @inspections.where("YEAR(ins_date) = ?", @selected_year)
+    #@inspections_by_month = @inspections.where("YEAR(ins_date) = ?", @selected_year)
                                         .group("MONTH(ins_date)")
                                         .order("MONTH(ins_date)")
                                         .count
-    @inspections_by_month = @inspections_by_month.transform_keys { |month| month_mapping[format('%02d', month)] }
-
+    #@inspections_by_month = @inspections_by_month.transform_keys { |month| month_mapping[format('%02d', month)] }
+    @inspections_by_month = @inspections
 
     # Agrupar inspecciones por año
-    @inspections_by_year = @inspections.group("YEAR(ins_date)").order("YEAR(ins_date)").count
-
+    #@inspections_by_year = @inspections.group("YEAR(ins_date)").order("YEAR(ins_date)").count
+    @inspections_by_year = @inspections
 
     # Gráfico de resultados de inspecciones
     @inspection_results = @principal.inspections
