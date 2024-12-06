e42c40a26f75ae8a42aa58d80223d8b1f975de04
joigs <jose.saez2001@alumnos.ubiobio.cl>
2024-12-06 14:17:23 -0300

    modificacion visual a checkbox

--- app/models/another.rb
+++ app/models/another.rb
@@ -1,30 +1,26 @@
 class Another < ApplicationRecord
-
-
-  serialize :level, type: Array,coder: JSON
+  serialize :level, type: Array, coder: JSON
   serialize :ins_type, type: Array, coder: JSON
 
   validates :point, presence: true
 
-  #validan que el array no este vacio
   validate :ins_type_array_presence
   validate :level_array_presence
-
-
 
   belongs_to :ruletype
   belongs_to :item
 
-
   private
 
-  #valida que el array no este vacio
   def ins_type_array_presence
-    errors.add(:"", 'Debe asignar el tipo de defecto') if ins_type.blank? || ins_type.all?(&:blank?)
+    if ins_type.blank? || ins_type.all?(&:blank?)
+      errors.add(:ins_type, 'Debe asignar el tipo de defecto')
+    end
   end
 
   def level_array_presence
-    errors.add(:"", 'Debe asignar el nivel de importancia') if level.blank? || level.all?(&:blank?)
+    if level.blank? || level.all?(&:blank?)
+      errors.add(:level, 'Debe asignar el nivel de importancia')
+    end
   end
-
 end
