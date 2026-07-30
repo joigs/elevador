f47736dde9b7843445beb4ebffad3b50e19606c0
joigs <jose.saez2001@alumnos.ubiobio.cl>
2026-07-30 12:23:28 -0400

    cambio

--- config/importmap.rb
+++ config/importmap.rb
@@ -14,6 +14,7 @@ pin "@popperjs/core", to: "@popperjs--core.js" # @2.11.8
 pin "flowbite-datepicker" # @1.3.0
 pin "chartkick", to: "chartkick.js"
 pin "Chart.bundle", to: "Chart.bundle.js"
-pin "sweetalert2" # @11.14.0
 pin "flatpickr", to: "https://cdn.jsdelivr.net/npm/flatpickr"
 pin "flatpickr/es", to: "https://cdn.jsdelivr.net/npm/flatpickr/dist/l10n/es.js"
+pin "sweetalert2",
+    to: "sweetalert2-neutral.js" # @11.22.2-neutral
